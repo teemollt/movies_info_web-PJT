44d0dce0589ed402c96cc8b8361b1f38124dda2d
teemollt <teemollt@gmail.com>
2021-05-28 00:53:44 +0900

    comment model relatedname

--- community/models.py
+++ community/models.py
@@ -15,7 +15,7 @@ class Article(models.Model):
 
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
     content = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
